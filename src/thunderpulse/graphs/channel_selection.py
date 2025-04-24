0a44b109b9eab0aa913d35985ace84e25e515230
wendtalexander <wendtalexander@protonmail.com>
2025-04-24 17:51:43 +0200

    [formattin]

--- src/thunderpulse/graphs/channel_selection.py
+++ src/thunderpulse/graphs/channel_selection.py
@@ -1,13 +1,14 @@
-from typing import Optional
-
 import nixio
 import numpy as np
 import numpy.typing as npt
 from IPython import embed
 
 
-def select_channels(channels: npt.NDArray, probe_selected_channels: Optional[dict], probe_frame:nixio.DataFrame):
-    
+def select_channels(
+    channels: npt.NDArray,
+    probe_selected_channels: dict | None,
+    probe_frame: nixio.DataFrame,
+):
     if channels.size == 1:
         channels = np.append(channels, channels[0])
 
@@ -18,11 +19,18 @@ def select_channels(channels: npt.NDArray, probe_selected_channels: Optional[dic
     channels = np.arange(channels[0], channels[1] + 1)
 
     sorted_after_y_pos = np.argsort(probe_frame["y"])
-    channels = probe_frame["contact_ids"][sorted_after_y_pos][channels].astype(np.int32) - 1
+    channels = (
+        probe_frame["contact_ids"][sorted_after_y_pos][channels].astype(
+            np.int32
+        )
+        - 1
+    )
 
     if probe_selected_channels:
         if probe_selected_channels["points"]:
-            channels = np.zeros(len(probe_selected_channels["points"]), dtype=np.int32)
+            channels = np.zeros(
+                len(probe_selected_channels["points"]), dtype=np.int32
+            )
             for i, items in enumerate(probe_selected_channels["points"]):
                 channel_id = items["text"].split(" ")[-1]
                 channels[i] = int(channel_id)
@@ -30,6 +38,5 @@ def select_channels(channels: npt.NDArray, probe_selected_channels: Optional[dic
             order = {key: i for i, key in enumerate(sorted_after_y_pos)}
             channels = np.array(sorted(channels, key=lambda d: order[d]))
             channel_length = len(channels)
-
 
     return channels.astype(np.int32)[::-1], channel_length
