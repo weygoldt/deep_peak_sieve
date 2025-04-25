e9dafc277e1b9716d11254da605c439bf7af7a77
wendtalexander <wendtalexander@protonmail.com>
2025-04-25 13:57:52 +0200

    [collections] removing filter to utils

--- src/thunderpulse/collection/filters.py
+++
@@ -1,9 +0,0 @@
-from scipy.signal import butter, sosfiltfilt
-
-
-def bandpass_filter(data, lowcut, highcut, sample_rate, order=2):
-    sos = butter(
-        order, [lowcut, highcut], btype="band", fs=sample_rate, output="sos"
-    )
-    y = sosfiltfilt(sos, data)
-    return y
