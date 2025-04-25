01252ecd9a5c2d85d9b05f2c209ac2b18ef5a47b
wendtalexander <wendtalexander@protonmail.com>
2025-04-25 16:13:57 +0200

    [formatting]

--- src/thunderpulse/processing/__init__.py
+++ src/thunderpulse/processing/__init__.py
@@ -1,6 +1,7 @@
-from . import peak_detection
-from . import save_processing
-from . import io
-from . import preprocessing
-from . import waveforms
-from . import calc_umap
+from . import (
+    calc_umap,
+    peak_detection,
+    preprocessing,
+    save_processing,
+    waveforms,
+)
