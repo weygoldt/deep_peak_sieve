5fb1738ceefe9da60866a1927413c9efcf6c8f20
wendtalexander <wendtalexander@protonmail.com>
2025-05-02 13:21:52 +0200

    [navbar] moving to card

--- src/thunderpulse/ui_layout/config_navbar/combine_navbars.py
+++ src/thunderpulse/ui_layout/config_navbar/combine_navbars.py
@@ -1,82 +1,96 @@
 import dash_bootstrap_components as dbc
 from dash import html
 
-from .cluster_card import create_cluster_card
-from .io_card import create_io_offcanvas
-from .peak_table_card import create_peak_table_card
-from .preprocessing_card import create_preprocessing_offcanvas
+from thunderpulse.ui_layout.config_navbar.filter_card import (
+    create_filter_offcanvas,
+)
+from thunderpulse.ui_layout.config_navbar.io_card import create_io_offcanvas
+from thunderpulse.ui_layout.config_navbar.pulse_detection import (
+    create_pulse_offcanvas,
+)
 
 
 def create_config_navbar():
     io_offcanvas = create_io_offcanvas()
-    preprocessing_offcanvas = create_preprocessing_offcanvas()
-    # peak_table_card = create_peak_table_card()
-    # cluster_card = create_cluster_card()
+    filter_offcanvas = create_filter_offcanvas()
+    pulse_offcanvas = create_pulse_offcanvas()
 
-    navbar = dbc.Container(
+    navbar = dbc.Card(
         [
-            dbc.Row(dbc.Col(html.A("Configurations")), justify="center"),
-            dbc.Row(
-                dbc.Navbar(
-                    dbc.Container(
+            dbc.CardHeader("Configurations", style={"textAlign": "center"}),
+            dbc.CardBody(
+                [
+                    dbc.Row(
                         [
-                            dbc.Row(
+                            dbc.Col(
                                 [
-                                    dbc.Col(
-                                        [
-                                            dbc.Button(
-                                                "Paths",
-                                                color="info",
-                                                id="bt_io_offcanvas",
-                                                n_clicks=0,
-                                            ),
-                                            dbc.Offcanvas(
-                                                io_offcanvas,
-                                                title="IO operations",
-                                                is_open=False,
-                                                id="io_offcanvas",
-                                                backdrop=False,
-                                            ),
-                                        ],
+                                    dbc.Button(
+                                        "Paths",
+                                        color="info",
+                                        id="bt_io_offcanvas",
+                                        n_clicks=0,
                                     ),
-                                    dbc.Col(
-                                        [
-                                            dbc.Button(
-                                                "Preprocessing",
-                                                color="info",
-                                                id="bt_preprocessing_offcanvas",
-                                                n_clicks=0,
-                                            ),
-                                            dbc.Offcanvas(
-                                                preprocessing_offcanvas,
-                                                title="Pulse detection preprocessing and detection parameter",
-                                                is_open=False,
-                                                id="preprocessing_offcanvas",
-                                                backdrop=False,
-                                                scrollable=True,
-                                            ),
-                                        ]
-                                    ),
-                                    dbc.Col(
-                                        dbc.Button(
-                                            "Load",
-                                            color="warning",
-                                            id="bt_load_data",
-                                            n_clicks=0,
-                                        ),
+                                    dbc.Offcanvas(
+                                        io_offcanvas,
+                                        title="IO operations",
+                                        is_open=False,
+                                        id="io_offcanvas",
+                                        backdrop=False,
                                     ),
                                 ],
-                                align="center",
-                                className="g-0",
+                            ),
+                            dbc.Col(
+                                [
+                                    dbc.Button(
+                                        "Filter",
+                                        color="info",
+                                        id="bt_filter_offcanvas",
+                                        n_clicks=0,
+                                    ),
+                                    dbc.Offcanvas(
+                                        filter_offcanvas,
+                                        title="Filter Parameters",
+                                        is_open=False,
+                                        id="filter_offcanvas",
+                                        backdrop=False,
+                                        scrollable=True,
+                                    ),
+                                ]
+                            ),
+                            dbc.Col(
+                                [
+                                    dbc.Button(
+                                        "Detection",
+                                        color="info",
+                                        id="bt_pulse_offcanvas",
+                                        n_clicks=0,
+                                    ),
+                                    dbc.Offcanvas(
+                                        pulse_offcanvas,
+                                        title="Pulse detection",
+                                        is_open=False,
+                                        id="pulse_offcanvas",
+                                        backdrop=False,
+                                        scrollable=True,
+                                    ),
+                                ]
+                            ),
+                            dbc.Col(
+                                dbc.Button(
+                                    "Load",
+                                    color="warning",
+                                    id="bt_load_data",
+                                    n_clicks=0,
+                                ),
                             ),
                         ],
+                        class_name="mx-auto",
                     ),
-                    color="dark",
-                    dark=True,
-                ),
-                align="center",
+                ]
             ),
-        ]
+        ],
+        color="dark",
+        inverse=True,
     )
 
     return navbar
