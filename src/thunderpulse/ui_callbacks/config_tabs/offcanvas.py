a332849168b8d3003ce241bc101338685c3678b3
wendtalexander <wendtalexander@protonmail.com>
2025-05-02 13:20:23 +0200

    [ui_callbacks] adding filter and detection

--- src/thunderpulse/ui_callbacks/config_tabs/offcanvas.py
+++ src/thunderpulse/ui_callbacks/config_tabs/offcanvas.py
@@ -13,13 +13,21 @@ def callbacks(app: Dash) -> None:
         return is_open
 
     @app.callback(
-        Output("preprocessing_offcanvas", "is_open"),
-        Input("bt_preprocessing_offcanvas", "n_clicks"),
-        State("preprocessing_offcanvas", "is_open"),
+        Output("filter_offcanvas", "is_open"),
+        Input("bt_filter_offcanvas", "n_clicks"),
+        State("filter_offcanvas", "is_open"),
     )
-    def toggle_preprocessing_offcanvas(
-        n_clicks: int | None, is_open: bool
-    ) -> bool:
+    def toggle_filter_offcanvas(n_clicks: int | None, is_open: bool) -> bool:
+        if n_clicks:
+            return not is_open
+        return is_open
+
+    @app.callback(
+        Output("pulse_offcanvas", "is_open"),
+        Input("bt_pulse_offcanvas", "n_clicks"),
+        State("pulse_offcanvas", "is_open"),
+    )
+    def toggle_pulse_offcanvas(n_clicks: int | None, is_open: bool) -> bool:
         if n_clicks:
             return not is_open
         return is_open
