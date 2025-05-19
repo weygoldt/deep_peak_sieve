80565876fc1d29a8a3d5486547eb13ac5d5a21a8
wendtalexander <wendtalexander@protonmail.com>
2025-05-19 17:02:33 +0200

    [waveforms] adding waveform plot

--- src/thunderpulse/ui_callbacks/graphs/waveforms.py
+++ src/thunderpulse/ui_callbacks/graphs/waveforms.py
@@ -3,75 +3,14 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from dash import Input, Output
+from IPython import embed
 from plotly import subplots
 
-from .channel_selection import select_channels
-
-
-def callback_waveforms(app):
-    @app.callback(
-        Output("waveforms", "figure"),
-        Input("vis_tabs", "active_tab"),
-        Input("channel_range_slider", "value"),
-        Input("filepath", "data"),
-        Input("probe", "selectedData"),
-        Input("waveform_rand_num", "value"),
-        Input("waveform_higher", "value"),
-        Input("waveform_lower", "value"),
-    )
-    def update_graph_waveforms(
-        tabs,
-        channels,
-        filepath,
-        probe_selected_channels,
-        wave_num,
-        higher,
-        lower,
-    ):
-        if tabs:
-            if not tabs == "tab_waveforms":
-                fig = default_waveforms_plot()
-                return fig
-        if not filepath:
-            fig = default_waveforms_plot()
-            return fig
-
-        DATA_PATH = filepath["data_path"]
-        if not DATA_PATH:
-            fig = default_waveforms_plot()
-            return fig
-        if not wave_num:
-            fig = default_waveforms_plot()
-            return fig
-
-        nix_file = nixio.File(filepath["data_path"], nixio.FileMode.ReadOnly)
-        block = nix_file.blocks[0]
-        section = nix_file.sections["recording"]
-        sample_rate = float(section["samplerate"][0])
-
-        if isinstance(channels, list):
-            channels = np.array(channels)
-
-        probe_frame = nix_file.blocks[0].data_frames["probe_frame"]
-        channels, channel_length = select_channels(
-            channels,
-            probe_selected_channels,
-            probe_frame,
-        )
-
-        fig = plot_waveforms(
-            block,
-            sample_rate,
-            channels,
-            channel_length,
-            wave_num,
-            higher,
-            lower,
-        )
-
-        nix_file.close()
-
-        return fig
+from thunderpulse.data_handling.data import load_data
+from thunderpulse.pulse_detection.config import Params
+from thunderpulse.pulse_detection.detection import detect_peaks_on_block
+from thunderpulse.ui_callbacks.graphs.channel_selection import select_channels
+from thunderpulse.ui_callbacks.graphs.data_selection import select_data
 
 
 def default_waveforms_plot():
@@ -90,71 +29,102 @@ def default_waveforms_plot():
     return fig
 
 
-def plot_waveforms(
-    block,
-    sample_rate,
-    channels,
-    channel_length,
-    wave_num,
-    higher,
-    lower,
-):
-    block_names = "".join([n.name for n in block.data_arrays])
-    if "waveform_channel_" not in block_names:
-        fig = default_waveforms_plot()
-        return fig
-
-    fig = subplots.make_subplots(
-        rows=channel_length,
-        shared_xaxes=True,
-        shared_yaxes="all",
+def callbacks(app):
+    @app.callback(
+        Output("waveforms", "figure"),
+        inputs={
+            "general": {
+                "filepath": Input("filepath", "data"),
+                "vis_tabs": Input("vis_tabs", "active_tab"),
+                "time_slider": Input("time_slider", "value"),
+                "channels": Input("channel_range_slider", "value"),
+                "probe_selected_channels": Input("probe", "selectedData"),
+            },
+            "pulse_detection_config": Input("pulse_detection_config", "data"),
+        },
     )
-    time_slice = np.arange(lower / 1000, higher / 1000, 1 / sample_rate)
-    colors = [*px.colors.qualitative.Light24, *px.colors.qualitative.Vivid]
+    def update_graph_waveforms(
+        general: dict,
+        pulse_detection_config,
+    ):
+        (
+            filepath,
+            tabs,
+            time_index,
+            channels,
+            probe_selected_channels,
+        ) = general.values()
 
-    # select randome waveforms
-    selection_index = {}
-    for ch in channels:
-        selection_index[ch] = []
-        wf_selection_pool = block.data_arrays[f"waveform_channel_{ch}"].shape[
-            0
-        ]
-        if wf_selection_pool < wave_num:
-            selection_index[ch] = np.arange(wf_selection_pool)
-        else:
-            selection_index[ch] = np.random.choice(
-                np.arange(wf_selection_pool), size=wave_num, replace=False
-            )
+        if tabs and tabs != "tab_waveforms":
+            return default_waveforms_plot()
+        if not filepath:
+            return default_waveforms_plot()
+        if not filepath["data_path"]:
+            return default_waveforms_plot()
 
-    [
-        fig.add_traces(
-            [
-                go.Scattergl(
-                    x=time_slice,
-                    y=block.data_arrays[f"waveform_channel_{ch}"][num],
-                    name=f"{ch}",
-                    mode="markers+lines",
-                    line_color=colors[ch],
-                    line_width=1,
-                    opacity=0.8,
-                )
-                for num in selection_index[ch]
-            ],
-            rows=i + 1,
-            cols=1,
+        d = load_data(**filepath)
+
+        params = Params.from_dict(pulse_detection_config)
+
+        channels = np.array(channels)
+
+        channels, channel_length = select_channels(
+            channels,
+            probe_selected_channels,
+            d.sensorarray,
         )
-        for i, ch in enumerate(channels)
-    ]
 
-    fig.update_layout(
-        showlegend=False,
-        clickmode="event+select",
-        autosize=True,
-        template="plotly_dark",
-        margin=dict(l=0, r=0, t=0, b=0),
-    )
-    [
-        fig.update_yaxes(range=[-200, 50], row=i + 1, col=1)
-        for i, _ in enumerate(channels)
-    ]
-    return fig
+        fig = subplots.make_subplots(
+            rows=channel_length,
+            shared_xaxes=True,
+            shared_yaxes="all",
+        )
+        time_display = 1.0
+
+        blockinfo = {
+            "blockiterval": 0,
+            "blocksize": int(1.0 * d.metadata.samplerate),
+            "overlap": 0,
+        }
+        sliced_data, time_slice = select_data(
+            d.data, time_index, time_display, d.metadata.samplerate
+        )
+        pulse_storage = detect_peaks_on_block(
+            sliced_data,
+            d.metadata.samplerate,
+            blockinfo,
+            params,
+        )
+        if not pulse_storage:
+            return default_waveforms_plot()
+
+        colors = [*px.colors.qualitative.Light24, *px.colors.qualitative.Vivid]
+        for i, ch in enumerate(channels, start=1):
+            pulse_index = pulse_storage["channels"] == ch
+            pulses = np.array(pulse_storage["pulses"])[pulse_index]
+
+            for p in pulses:
+                fig.add_trace(
+                    go.Scattergl(
+                        x=np.arange(p.shape[0]) / d.metadata.samplerate
+                        - params.peaks.cutout_window_around_peak_s,
+                        y=p,
+                        mode="lines",
+                        line_color=colors[ch],
+                        name=f"Peaks {ch}",
+                        line_width=1,
+                        opacity=0.8,
+                    ),
+                    row=i,
+                    col=[1] * channel_length,
+                )
+
+        fig.update_layout(
+            showlegend=False,
+            clickmode="event+select",
+            autosize=True,
+            template="plotly_dark",
+            margin=dict(l=0, r=0, t=0, b=0),
+        )
+
+        return fig
