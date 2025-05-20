e962a2571b629dbd2c899dafa7475f699e55ea41
wendtalexander <wendtalexander@protonmail.com>
2025-05-20 16:19:45 +0200

    [dsp] adding get_common_median_ref

--- src/thunderpulse/dsp/common_reference.py
+++ src/thunderpulse/dsp/common_reference.py
@@ -5,5 +5,9 @@ def common_median_reference(data):
     return data - np.median(data, axis=1, keepdims=True)
 
 
+def get_common_median_reference(data):
+    return np.median(data, axis=1, keepdims=True)
+
+
 def common_mean_reference(data):
     return data - np.mean(data, axis=1, keepdims=True)
